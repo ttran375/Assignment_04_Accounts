5bd40c86100330e8f55d8281b3f05c24927522cc
Dat Tran <datttrian@gmail.com>
2024-03-23 18:12:51 +0000

    Sat, Mar 23, 2024, 6:12 PM +00:00

--- Program.cs
+++ Program.cs
@@ -214,7 +214,7 @@ public abstract class Account
     private static int LAST_NUMBER = 100_000;
 
     protected readonly List<Person> users;
-    protected readonly List<Transaction> transactions;
+    public readonly List<Transaction> transactions;
 
     public event EventHandler<TransactionEventArgs> OnTransaction;
 
@@ -253,7 +253,7 @@ public abstract class Account
         return users.Any(user => user.Name == name);
     }
 
-    public abstract void PrepareMonthlyStatement();
+    public abstract void PrepareMonthlyReport();
 
     protected void RaiseOnTransaction(TransactionEventArgs args)
     {
@@ -329,7 +329,7 @@ public class CheckingAccount : Account, ITransaction
     }
 
     // Override PrepareMonthlyReport to handle checking account specifics
-    public override void PrepareMonthlyStatement()
+    public override void PrepareMonthlyReport()
     {
         double serviceCharge = transactions.Count * COST_PER_TRANSACTION;
         double interest = (LowestBalance * INTEREST_RATE) / 12;
@@ -384,7 +384,7 @@ public class SavingAccount : Account, ITransaction
     }
 
     // Override PrepareMonthlyReport to handle saving account specifics
-    public override void PrepareMonthlyStatement()
+    public override void PrepareMonthlyReport()
     {
         double serviceCharge = transactions.Count * COST_PER_TRANSACTION;
         double interest = (LowestBalance * INTEREST_RATE) / 12;
@@ -441,7 +441,7 @@ public class VisaAccount : Account, ITransaction
     }
 
     // Override PrepareMonthlyReport to calculate and apply interest
-    public override void PrepareMonthlyStatement()
+    public override void PrepareMonthlyReport()
     {
         double interest = (LowestBalance * INTEREST_RATE) / 12;
         Balance -= interest; // Subtracting interest increases the debt
@@ -453,4 +453,508 @@ public class VisaAccount : Account, ITransaction
         DoPurchase(amount, person);
     }
 
+    public void Pay(double amount, Person person)
+    {
+        if (!IsUser(person.Name))
+        {
+            RaiseOnTransaction(new TransactionEventArgs(person.Name, amount, false));
+            throw new AccountException(ExceptionType.NAME_NOT_ASSOCIATED_WITH_ACCOUNT);
+        }
+        else if (!person.IsAuthenticated)
+        {
+            RaiseOnTransaction(new TransactionEventArgs(person.Name, amount, false));
+            throw new AccountException(ExceptionType.USER_NOT_LOGGED_IN);
+        }
+        else if (amount < 0)
+        {
+            RaiseOnTransaction(new TransactionEventArgs(person.Name, amount, false));
+            throw new ArgumentException("Payment amount must be positive");
+        }
+        else
+        {
+            base.Deposit(-amount, person); // Decrease the balance (repay debt)
+            RaiseOnTransaction(new TransactionEventArgs(person.Name, -amount, true));
+        }
+    }
+
+    public void Purchase(double amount, Person person)
+    {
+        if (!IsUser(person.Name))
+        {
+            RaiseOnTransaction(new TransactionEventArgs(person.Name, -amount, false));
+            throw new AccountException(ExceptionType.NAME_NOT_ASSOCIATED_WITH_ACCOUNT);
+        }
+        else if (!person.IsAuthenticated)
+        {
+            RaiseOnTransaction(new TransactionEventArgs(person.Name, -amount, false));
+            throw new AccountException(ExceptionType.USER_NOT_LOGGED_IN);
+        }
+        else if (amount < 0)
+        {
+            RaiseOnTransaction(new TransactionEventArgs(person.Name, -amount, false));
+            throw new ArgumentException("Purchase amount must be positive");
+        }
+        else if (Balance + amount > creditLimit)
+        {
+            RaiseOnTransaction(new TransactionEventArgs(person.Name, -amount, false));
+            throw new AccountException(ExceptionType.CREDIT_LIMIT_HAS_BEEN_EXCEEDED);
+        }
+        else
+        {
+            base.Deposit(-amount, person); // Decrease the balance (increase debt)
+            RaiseOnTransaction(new TransactionEventArgs(person.Name, -amount, true));
+        }
+    }
+
+}
+
+public static class Bank
+{
+    public static readonly Dictionary<string, Account> ACCOUNTS = new Dictionary<string, Account>();
+    public static readonly Dictionary<string, Person> USERS = new Dictionary<string, Person>();
+
+    static Bank()
+    {
+        //initialize the USERS collection
+
+        AddPerson("Narendra", "1234-5678"); //0
+
+        AddPerson("Ilia", "2345-6789"); //1
+
+        AddPerson("Mehrdad", "3456-7890"); //2
+
+        AddPerson("Vijay", "4567-8901"); //3
+
+        AddPerson("Arben", "5678-9012"); //4
+
+        AddPerson("Patrick", "6789-0123"); //5
+
+        AddPerson("Yin", "7890-1234"); //6
+
+        AddPerson("Hao", "8901-2345"); //7
+
+        AddPerson("Jake", "9012-3456"); //8
+
+        AddPerson("Mayy", "1224-5678"); //9
+
+        AddPerson("Nicoletta", "2344-6789"); //10
+
+        //initialize the ACCOUNTS collection
+
+        AddAccount(new VisaAccount()); //VS-100000
+
+        AddAccount(new VisaAccount(150, -500)); //VS-100001
+
+        AddAccount(new SavingAccount(5000)); //SV-100002
+
+        AddAccount(new SavingAccount()); //SV-100003
+
+        AddAccount(new CheckingAccount(2000)); //CK-100004
+
+        AddAccount(new CheckingAccount(1500, true));//CK-100005
+
+        AddAccount(new VisaAccount(50, -550)); //VS-100006
+
+        AddAccount(new SavingAccount(1000)); //SV-100007
+
+        //associate users with accounts
+
+        string number = "VS-100000";
+
+        AddUserToAccount(number, "Narendra");
+
+        AddUserToAccount(number, "Ilia");
+
+        AddUserToAccount(number, "Mehrdad");
+
+        number = "VS-100001";
+
+        AddUserToAccount(number, "Vijay");
+
+        AddUserToAccount(number, "Arben");
+
+        AddUserToAccount(number, "Patrick");
+
+        number = "SV-100002";
+
+        AddUserToAccount(number, "Yin");
+
+        AddUserToAccount(number, "Hao");
+
+        AddUserToAccount(number, "Jake");
+
+        number = "SV-100003";
+
+        AddUserToAccount(number, "Mayy");
+
+        AddUserToAccount(number, "Nicoletta");
+
+        number = "CK-100004";
+
+        AddUserToAccount(number, "Mehrdad");
+
+        AddUserToAccount(number, "Arben");
+
+        AddUserToAccount(number, "Yin");
+
+        number = "CK-100005";
+
+        AddUserToAccount(number, "Jake");
+
+        AddUserToAccount(number, "Nicoletta");
+
+        number = "VS-100006";
+
+        AddUserToAccount(number, "Ilia");
+
+        AddUserToAccount(number, "Vijay");
+
+        number = "SV-100007";
+
+        AddUserToAccount(number, "Patrick");
+
+        AddUserToAccount(number, "Hao");
+    }
+
+    public static void AddPerson(string name, string sin)
+    {
+        Person person = new Person(name, sin);
+        person.OnLogin += Logger.LoginHandler;
+        USERS.Add(name, person);
+    }
+
+    public static void AddAccount(Account account)
+    {
+        account.OnTransaction += Logger.TransactionHandler;
+        ACCOUNTS.Add(account.Number, account);
+    }
+
+    public static void AddUserToAccount(string number, string name)
+    {
+        if (ACCOUNTS.ContainsKey(number) && USERS.ContainsKey(name))
+        {
+            Account account = ACCOUNTS[number];
+            Person person = USERS[name];
+            account.AddUser(person);
+        }
+        else
+        {
+            throw new AccountException(ExceptionType.ACCOUNT_DOES_NOT_EXIST);
+        }
+    }
+
+    public static void PrintAccounts()
+    {
+        foreach (var account in ACCOUNTS)
+        {
+            Console.WriteLine(account.Value);
+        }
+    }
+
+    public static void PrintPersons()
+    {
+        foreach (var user in USERS)
+        {
+            Console.WriteLine(user.Value);
+        }
+    }
+
+    public static Person GetPerson(string name)
+    {
+        if (USERS.ContainsKey(name))
+        {
+            return USERS[name];
+        }
+        else
+        {
+            throw new AccountException(ExceptionType.USER_DOES_NOT_EXIST);
+        }
+    }
+
+    public static Account GetAccount(string number)
+    {
+        if (ACCOUNTS.ContainsKey(number))
+        {
+            return ACCOUNTS[number];
+        }
+        else
+        {
+            throw new AccountException(ExceptionType.ACCOUNT_DOES_NOT_EXIST);
+        }
+    }
+
+    public static List<Transaction> GetAllTransactions()
+    {
+        List<Transaction> allTransactions = new List<Transaction>();
+        foreach (var account in ACCOUNTS)
+        {
+            allTransactions.AddRange(account.Value.transactions);
+        }
+        return allTransactions;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("\nAll acounts:");
+
+        Bank.PrintAccounts();
+
+        Console.WriteLine("\nAll Users:");
+
+        Bank.PrintPersons();
+
+        Person p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10;
+
+        p0 = Bank.GetPerson("Narendra");
+
+        p1 = Bank.GetPerson("Ilia");
+
+        p2 = Bank.GetPerson("Mehrdad");
+
+        p3 = Bank.GetPerson("Vijay");
+
+        p4 = Bank.GetPerson("Arben");
+
+        p5 = Bank.GetPerson("Patrick");
+
+        p6 = Bank.GetPerson("Yin");
+
+        p7 = Bank.GetPerson("Hao");
+
+        p8 = Bank.GetPerson("Jake");
+
+        p9 = Bank.GetPerson("Mayy");
+
+        p10 = Bank.GetPerson("Nicoletta");
+
+        p0.Login("123"); p1.Login("234");
+
+        p2.Login("345"); p3.Login("456");
+
+        p4.Login("567"); p5.Login("678");
+
+        p6.Login("789"); p7.Login("890");
+
+        p10.Login("234"); p8.Login("901");
+
+        //a visa account
+
+        VisaAccount a = Bank.GetAccount("VS-100000") as VisaAccount;
+
+        a.Pay(1500, p0);
+
+        a.Purchase(200, p1);
+
+        a.Purchase(25, p2);
+
+        a.Purchase(15, p0);
+
+        a.Purchase(39, p1);
+
+        a.Pay(400, p0);
+
+        Console.WriteLine(a);
+
+        a = Bank.GetAccount("VS-100001") as VisaAccount;
+
+        a.Pay(500, p0);
+
+        a.Purchase(25, p3);
+
+        a.Purchase(20, p4);
+
+        a.Purchase(15, p5);
+
+        Console.WriteLine(a);
+
+        //a saving account
+
+        SavingAccount b = Bank.GetAccount("SV-100002") as SavingAccount;
+
+        b.Withdraw(300, p6);
+
+        b.Withdraw(32.90, p6);
+
+        b.Withdraw(50, p7);
+
+        b.Withdraw(111.11, p8);
+
+        Console.WriteLine(b);
+
+        b = Bank.GetAccount("SV-100003") as SavingAccount;
+
+        b.Deposit(300, p3); //ok even though p3 is not a holder
+
+        b.Deposit(32.90, p2);
+
+        b.Deposit(50, p5);
+
+        b.Withdraw(111.11, p10);
+
+        Console.WriteLine(b);
+
+        //a checking account
+
+        CheckingAccount c = Bank.GetAccount("CK-100004") as CheckingAccount;
+
+        c.Deposit(33.33, p7);
+
+        c.Deposit(40.44, p7);
+
+        c.Withdraw(150, p2);
+
+        c.Withdraw(200, p4);
+
+        c.Withdraw(645, p6);
+
+        c.Withdraw(350, p6);
+
+        Console.WriteLine(c);
+
+        c = Bank.GetAccount("CK-100005") as CheckingAccount;
+
+        c.Deposit(33.33, p8);
+
+        c.Deposit(40.44, p7);
+
+        c.Withdraw(450, p10);
+
+        c.Withdraw(500, p8);
+
+        c.Withdraw(645, p10);
+
+        c.Withdraw(850, p10);
+
+        Console.WriteLine(c);
+
+        a = Bank.GetAccount("VS-100006") as VisaAccount;
+
+        a.Pay(700, p0);
+
+        a.Purchase(20, p3);
+
+        a.Purchase(10, p1);
+
+        a.Purchase(15, p1);
+
+        Console.WriteLine(a);
+
+        b = Bank.GetAccount("SV-100007") as SavingAccount;
+
+        b.Deposit(300, p3); //ok even though p3 is not a holder
+
+        b.Deposit(32.90, p2);
+
+        b.Deposit(50, p5);
+
+        b.Withdraw(111.11, p7);
+
+        Console.WriteLine(b);
+
+        Console.WriteLine("\n\nExceptions:");
+
+        //The following will cause exception
+
+        try
+
+        {
+
+            p8.Login("911"); //incorrect password
+
+        }
+
+        catch (AccountException e) { Console.WriteLine(e.Message); }
+
+        try
+
+        {
+
+            p3.Logout();
+
+            a.Purchase(12.5, p3); //exception user is not logged in
+
+        }
+
+        catch (AccountException e) { Console.WriteLine(e.Message); }
+
+        try
+
+        {
+
+            a.Purchase(12.5, p0); //user is not associated with this account
+
+        }
+
+        catch (AccountException e) { Console.WriteLine(e.Message); }
+
+        try
+
+        {
+
+            a.Purchase(5825, p4); //credit limit exceeded
+
+        }
+
+        catch (AccountException e) { Console.WriteLine(e.Message); }
+
+        try
+
+        {
+
+            c.Withdraw(1500, p6); //no overdraft
+
+        }
+
+        catch (AccountException e) { Console.WriteLine(e.Message); }
+
+        try
+
+        {
+
+            Bank.GetAccount("CK-100018"); //account does not exist
+
+        }
+
+        catch (AccountException e) { Console.WriteLine(e.Message); }
+
+        try
+
+        {
+
+            Bank.GetPerson("Trudeau"); //user does not exist
+
+        }
+
+        catch (AccountException e) { Console.WriteLine(e.Message); }
+
+        //show all transactions
+
+        Console.WriteLine("\n\nAll transactions");
+
+        foreach (var transaction in Bank.GetAllTransactions())
+
+            Console.WriteLine(transaction);
+
+        foreach (var keyValuePair in Bank.ACCOUNTS)
+
+        {
+
+            Account account = keyValuePair.Value;
+
+            Console.WriteLine("\nBefore PrepareMonthlyReport()");
+
+            Console.WriteLine(account);
+
+            Console.WriteLine("\nAfter PrepareMonthlyReport()");
+
+            account.PrepareMonthlyReport(); //all transactions are cleared, balance changes
+
+            Console.WriteLine(account);
+
+        }
+
+        Logger.ShowLoginEvents();
+
+        Logger.ShowTransactionEvents();
+    }
 }
